5d05f9998bec4a9b50809b38c7e1319889ff454c
sknabirasool <sknabirasool104@gmail.com>
2025-08-27 15:01:50 +0530

    save

--- app/Mail/ProjectRegistrationMail.php
+++ app/Mail/ProjectRegistrationMail.php
@@ -21,41 +21,13 @@ class ProjectRegistrationMail extends Mailable
     public function __construct($user, $data)
     {
 
-          $this->user=$user;
-            $this->data=$data;
+        $this->user=$user;
+        $this->data=$data;
     }
 
       public function build()
     {
         return $this->subject('Welcome To Build Your App || Project Registration Mail')->view('emails.project-register-mail',['user' => $this->user, 'data' => $this->data]);
     }
-    /**
-     * Get the message envelope.
-     */
-    public function envelope(): Envelope
-    {
-        return new Envelope(
-            subject: 'Project Registration Mail',
-        );
-    }
 
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'view.name',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
-    }
 }
